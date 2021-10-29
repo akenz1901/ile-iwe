4a1456d00e87329af2c7b6e6cdbc3a762e4f419f
akenz1901 <michael_aka1@icloud.com>
2021-10-29 14:17:03 +0100

    Learning Party refactored

--- src/main/java/com/ileiwe/data/model/LearningParty.java
+++ src/main/java/com/ileiwe/data/model/LearningParty.java
@@ -1,26 +1,51 @@
 package com.ileiwe.data.model;
 
-import lombok.Data;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class LearningParty {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(unique = true, nullable = false)
+    @NotNull @NotBlank
     private String email;
     @Column(unique = true, nullable = false)
+    @NotNull @NotBlank
     private String password;
     private boolean enabled;
     @CreationTimestamp
     private LocalDateTime dateCreated;
-    @OneToMany
+    @OneToMany(cascade = CascadeType.PERSIST)
     private List<Authority> authorities;
 
+    public LearningParty(String email, String password, Authority authority){
+//        if(email.strip().isEmpty() || password.strip().isEmpty()){
+//            throw new IllegalArgumentException("email and password is required");
+//        }
+        this.email = email;
+        this.password = password;
+        addAuthority(authority);
+        this.enabled = false;
+    }
+
+    public void addAuthority(Authority authority){
+        if(this.authorities == null){
+            this.authorities = new ArrayList<>();
+        }
+        this.authorities.add(authority);
+    }
 }
