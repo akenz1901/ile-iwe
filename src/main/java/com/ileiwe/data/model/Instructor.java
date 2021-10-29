8d867fad3ed8c3a8891e99ccab9ae9ab88c196f3
akenz1901 <michael_aka1@icloud.com>
2021-10-29 14:14:31 +0100

    Instructor Contstraint modified

--- src/main/java/com/ileiwe/data/model/Instructor.java
+++ src/main/java/com/ileiwe/data/model/Instructor.java
@@ -1,24 +1,37 @@
 package com.ileiwe.data.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Entity
 @Data
+@Builder()
+@AllArgsConstructor
+@NoArgsConstructor
 public class Instructor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull @NotBlank
+    @Column(nullable = false)
     private String firstname;
+    @NotNull @NotBlank
+    @Column(nullable = false)
     private String lastname;
     @Column(length = 1000)
     private String bio;
+    @Enumerated(EnumType.STRING)
     private Gender gender;
     private String specialization;
-    @OneToOne
+    @OneToOne(cascade = CascadeType.PERSIST)
     private LearningParty learningParty;
-    @OneToMany
+    @OneToMany(cascade = CascadeType.PERSIST)
     private List<Course> courseList;
 }
