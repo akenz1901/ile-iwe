84d8fb0a7c8a6d22c89c8d409641b6ce1d7102bd
akenz1901 <michael_aka1@icloud.com>
2021-10-29 14:22:32 +0100

    Instructor Repository also extends JPARepository

--- src/main/java/com/ileiwe/data/repository/InstructorRepository.java
+++ src/main/java/com/ileiwe/data/repository/InstructorRepository.java
@@ -1,2 +1,8 @@
-package com.ileiwe.data.repository;public interface InstructorRepository {
+package com.ileiwe.data.repository;
+
+import com.ileiwe.data.model.Instructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InstructorRepository extends JpaRepository<Instructor, Long> {
+
 }
