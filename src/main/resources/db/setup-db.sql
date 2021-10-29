2b737bfdd1fb314e86f0cac53d8b9df6c288e455
akenz1901 <michael_aka1@icloud.com>
2021-10-29 14:15:00 +0100

    Modefied the DB Script

--- src/main/resources/db/setup-db.sql
+++ src/main/resources/db/setup-db.sql
@@ -1,4 +1,4 @@
 create database ileiwedb;
-create user 'ileiwe_user'@'localhost' identified by 'Ileiwe111';
+create user if not exists 'ileiwe_user'@'localhost' identified by 'Ileiwe111';
 grant all privileges on ileiwedb.* to 'ileiwe_user'@'localhost';
 flush privileges;
