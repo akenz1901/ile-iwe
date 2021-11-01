11145ec0bbbc9c515c03f433aec47b8600603988
akenz1901 <michael_aka1@icloud.com>
2021-11-01 07:55:47 +0100

    Service Implementation for Instructor Service

--- src/main/java/com/ileiwe/service/intructor/InstructorServiceImpl.java
+++ src/main/java/com/ileiwe/service/intructor/InstructorServiceImpl.java
@@ -1,2 +1,34 @@
-package com.ileiwe.service;public class InstructorServiceImpl {
+package com.ileiwe.service.intructor;
+
+import com.ileiwe.data.dto.InstructorPartyDto;
+import com.ileiwe.data.model.Authority;
+import com.ileiwe.data.model.Instructor;
+import com.ileiwe.data.model.LearningParty;
+import com.ileiwe.data.model.Role;
+import com.ileiwe.data.repository.InstructorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InstructorServiceImpl implements InstructorService{
+
+    @Autowired
+    InstructorRepository instructorRepository;
+
+    @Override
+    public Instructor save(InstructorPartyDto instructorDto) {
+
+        if (instructorDto == null){
+            throw new IllegalArgumentException("Instructor can't be null");
+        }
+        LearningParty learningPartyUser =
+                new LearningParty(instructorDto.getEmail(), instructorDto.getPassword(),
+                        new Authority(Role.ROLE_INSTRUCTOR));
+
+        Instructor instructor = Instructor.builder()
+                .lastname(instructorDto.getLastname())
+                .firstname(instructorDto.getFirstname())
+                .learningParty(learningPartyUser).build();
+        return instructorRepository.save(instructor);
+    }
 }
