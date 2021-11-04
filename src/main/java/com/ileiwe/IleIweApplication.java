f1b6ec87d2ccd0516c091d28941699573b43c6f1
akenz1901 <michael_aka1@icloud.com>
2021-11-04 19:52:40 +0100

    Bean can't be wired while it's being initialized

--- src/main/java/com/ileiwe/IleIweApplication.java
+++ src/main/java/com/ileiwe/IleIweApplication.java
@@ -2,12 +2,19 @@ package com.ileiwe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class IleIweApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IleIweApplication.class, args);
+
 	}
 
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
 }
--- src/main/java/com/ileiwe/service/intructor/InstructorServiceImpl.java
+++ src/main/java/com/ileiwe/service/intructor/InstructorServiceImpl.java
@@ -7,6 +7,7 @@ import com.ileiwe.data.model.LearningParty;
 import com.ileiwe.data.model.Role;
 import com.ileiwe.data.repository.InstructorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +16,8 @@ public class InstructorServiceImpl implements InstructorService{
     @Autowired
     InstructorRepository instructorRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
     @Override
     public Instructor save(InstructorPartyDto instructorDto) {
 
@@ -22,13 +25,13 @@ public class InstructorServiceImpl implements InstructorService{
             throw new IllegalArgumentException("Instructor can't be null");
         }
         LearningParty learningPartyUser =
-                new LearningParty(instructorDto.getEmail(), instructorDto.getPassword(),
+                new LearningParty(instructorDto.getEmail(), bCryptPasswordEncoder.encode(instructorDto.getPassword()),
                         new Authority(Role.ROLE_INSTRUCTOR));
 
         Instructor instructor = Instructor.builder()
                 .lastname(instructorDto.getLastname())
                 .firstname(instructorDto.getFirstname())
                 .learningParty(learningPartyUser).build();
-        return instructorRepository.save(instructor);
+         return instructorRepository.save(instructor);
     }
 }
