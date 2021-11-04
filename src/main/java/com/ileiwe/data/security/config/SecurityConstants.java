1547a61f46a4ccc6bb19c95e767e8c640d9770ab
akenz1901 <michael_aka1@icloud.com>
2021-11-04 20:08:03 +0100

    Security constants are indeed constant and they helps us keep session Information including the token key

--- src/main/java/com/ileiwe/data/security/config/SecurityConstants.java
+++ src/main/java/com/ileiwe/data/security/config/SecurityConstants.java
@@ -1,2 +1,8 @@
-package com.ileiwe.data.security.config;public class SecurityConstants {
+package com.ileiwe.data.security.config;
+
+public class SecurityConstants {
+    public static final String SECRET = "SecretKeyToGen";
+    public static final long EXPIRATION_TIME = 845_000_000;//10 days
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
 }
