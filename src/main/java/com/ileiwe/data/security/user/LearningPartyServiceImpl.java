b4e1a1ea7188240aff2eaaef1550f26dd3bf4766
akenz1901 <michael_aka1@icloud.com>
2021-11-04 20:01:09 +0100

    Service saves Instructor

--- src/main/java/com/ileiwe/data/security/user/LearningPartyServiceImpl.java
+++ src/main/java/com/ileiwe/data/security/user/LearningPartyServiceImpl.java
@@ -3,22 +3,26 @@ package com.ileiwe.data.security.user;
 import com.ileiwe.data.model.Authority;
 import com.ileiwe.data.model.LearningParty;
 import com.ileiwe.data.repository.LearningPartyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class LearningPartyServiceImpl implements UserDetailsService {
 
+    @Autowired
     private LearningPartyRepository learningPartyRepository;
-
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
