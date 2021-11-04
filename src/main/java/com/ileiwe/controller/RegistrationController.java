76f2f470ce128c97d8c4628ceffbd2f908a7fd93
akenz1901 <michael_aka1@icloud.com>
2021-11-04 19:54:01 +0100

    registration controller

--- src/main/java/com/ileiwe/controller/RegistrationController.java
+++ src/main/java/com/ileiwe/controller/RegistrationController.java
@@ -1,2 +1,21 @@
-package com.ileiwe.controller;public class RegistrationController {
+package com.ileiwe.controller;
+
+import com.ileiwe.data.dto.InstructorPartyDto;
+import com.ileiwe.service.intructor.InstructorServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class RegistrationController {
+
+    @Autowired
+    private InstructorServiceImpl instructorService;
+
+    @PostMapping("/instructor")
+    public ResponseEntity<?> registerAsInstructor(@RequestBody InstructorPartyDto instructorPartyDto){
+        return ResponseEntity.ok()
+                .body(instructorService.save(instructorPartyDto));
+    }
 }
