f2c2162030b41be9b6014087e1820c0c1be82118
akenz1901 <michael_aka1@icloud.com>
2021-11-08 22:49:37 +0100

    Tested that we can send message using mail gun servcie

--- src/test/java/com/ileiwe/service/mail/MailGunServiceImplTest.java
+++ src/test/java/com/ileiwe/service/mail/MailGunServiceImplTest.java
@@ -1,4 +1,39 @@
+package com.ileiwe.service.mail;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class MailGunServiceImplTest {
-  
+    
+    @Autowired
+    @Qualifier("mailgun")
+    EmailService emailService;
+
+    @Test
+    void sendEmailWithMailGunTest(){
+        Message mail = Message.builder()
+                .from("akenz1901@gmail.com")
+                .to("michael_aka1@icloud.com")
+                .subject("Testing HTTP")
+                .body("Hello Bro").build();
+
+        MailResponse response = emailService.send(mail);
+        assertTrue(response.isSuccessful());
+    }
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
 }
