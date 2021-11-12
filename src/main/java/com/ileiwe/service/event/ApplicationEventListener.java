ee15f537badbf49b9c3eb5f0d9c3e01e6926ced2
akenz1901 <michael_aka1@icloud.com>
2021-11-12 23:41:32 +0100

    Registration momentopry

--- src/main/java/com/ileiwe/service/event/ApplicationEventListener.java
+++ src/main/java/com/ileiwe/service/event/ApplicationEventListener.java
@@ -1,2 +1,36 @@
-package com.ileiwe.service.event;public class ApplicationEventListener {
+package com.ileiwe.service.event;
+
+import com.ileiwe.service.mail.EmailService;
+import com.ileiwe.service.mail.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.UUID;
+
+@Component
+public class ApplicationEventListener {
+    @Autowired
+    EmailService emailService;
+
+    @Autowired
+    TemplateEngine templateEngine;
+    @EventListener
+    public void handleRegistrationCompleteEvent(OnRegistrationCompleteEvent onRegistrationCompleteEvent){
+        this.confirmRegistration(onRegistrationCompleteEvent);
+    }
+    private void confirmRegistration(OnRegistrationCompleteEvent event){
+        String verification = UUID.randomUUID().toString();
+
+        Message message = Message.builder()
+                .from("akenz1901@gmail.com")
+                .to(event.getAppUser().getEmail())
+                .subject("Confirmation Email")
+                .body(templateEngine.process("confirmation", new Context()))
+                .build();
+        emailService.send(message);
+
+    }
 }
--- src/main/java/com/ileiwe/service/event/OnRegistrationCompleteEvent.java
+++ src/main/java/com/ileiwe/service/event/OnRegistrationCompleteEvent.java
@@ -1,2 +1,16 @@
-package com.ileiwe.service.event;public class OnRegistrationCompleteEvent {
+package com.ileiwe.service.event;
+
+import com.ileiwe.data.model.LearningParty;
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
+
+import java.time.Clock;
+
+@Getter
+public class OnRegistrationCompleteEvent extends ApplicationEvent {
+    private LearningParty appUser;
+    public OnRegistrationCompleteEvent(LearningParty source) {
+        super(source);
+        this.appUser = source;
+    }
 }
