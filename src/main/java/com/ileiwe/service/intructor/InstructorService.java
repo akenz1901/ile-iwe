9a3e885d81872f6308c8bb03f948d7b293d512fb
akenz1901 <michael_aka1@icloud.com>
2021-11-12 23:45:14 +0100

    Service of interuct it

--- src/main/java/com/ileiwe/service/intructor/InstructorService.java
+++ src/main/java/com/ileiwe/service/intructor/InstructorService.java
@@ -2,7 +2,8 @@ package com.ileiwe.service.intructor;
 
 import com.ileiwe.data.dto.InstructorPartyDto;
 import com.ileiwe.data.model.Instructor;
+import com.ileiwe.service.exception.UserAlreadyExistException;
 
 public interface InstructorService {
-    Instructor save(InstructorPartyDto instructor);
+    Instructor save(InstructorPartyDto instructor) throws UserAlreadyExistException;
 }
